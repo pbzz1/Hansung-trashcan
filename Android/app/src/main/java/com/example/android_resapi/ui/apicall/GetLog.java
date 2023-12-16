d415aab6f82082c71d87c558d553b556bfe627a6
smilehee18 <123307856+smilehee18@users.noreply.github.com>
2023-12-16 13:48:00 +0900

    Update GetLog.java

--- Android/app/src/main/java/com/example/android_resapi/ui/apicall/GetLog.java
+++ Android/app/src/main/java/com/example/android_resapi/ui/apicall/GetLog.java
@@ -38,24 +38,25 @@ public class GetLog extends GetRequest {
     @Override
     protected void onPreExecute() {
         try {
-
+            //사용자가 택한 dialog 날짜와 시간대로 뷰를 세팅
             TextView textView_Date1 = activity.findViewById(R.id.textView_date1);
             TextView textView_Time1 = activity.findViewById(R.id.textView_time1);
             TextView textView_Date2 = activity.findViewById(R.id.textView_date2);
             TextView textView_Time2 = activity.findViewById(R.id.textView_time2);
 
+            //url 쿼리문 만들어줌 
             String params = String.format("?from=%s:00&to=%s:00",textView_Date1.getText().toString()+textView_Time1.getText().toString(),
                                                             textView_Date2.getText().toString()+textView_Time2.getText().toString());
 
-            Log.i(TAG,"urlStr="+urlStr+params);
+            Log.i(TAG,"urlStr="+urlStr+params); //url 잘 만들어졌는지 테스트 프린트 
             url = new URL(urlStr+params);
 
-        } catch (MalformedURLException e) {
+        } catch (MalformedURLException e) { //url 예외처리 
             Toast.makeText(activity,"URL is invalid:"+urlStr, Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
         TextView message = activity.findViewById(R.id.message2);
-        message.setText("조회중...");
+        message.setText("조회중..."); //조회 시간 지연 고려한 텍스트 
     }
 
     @Override
@@ -85,19 +86,19 @@ public class GetLog extends GetRequest {
         //chart 액티비티에서 호출했다면 차트 띄움
         if(activity instanceof ChartActivity)
         {
-
             LineChart chart = (LineChart)activity.findViewById(R.id.chart);
+            //비율값들 추가할 ratioValue arrayList 생성
             ArrayList <Entry> ratioValue = new ArrayList<>();
 
             for (Tag item : output) {
                 // 각 항목에서 필요한 데이터 추출
-                float timestamp = Float.parseFloat(item.Time);
-                float sound = Float.parseFloat(item.Ratio);
-                ratioValue.add(new Entry(timestamp, sound));
+                float timestamp = Float.parseFloat(item.Time); //Time값 밀리 세컨드
+                float ratio = Float.parseFloat(item.Ratio); //비율 값 -> float형변환
+                ratioValue.add(new Entry(timestamp, ratio)); //ratioValue ArrayList에 (시간, 비율) 값들 for문 돌면서 저장 
             }
             // LineDataSet 객체 생성
-            LineDataSet set = new LineDataSet(ratioValue, "Ratio");
-            set.setDrawIcons(false);
+            LineDataSet set = new LineDataSet(ratioValue, "Ratio"); //데이터셋으로 앞서 선언한 arrayList 쓰겠다고 정의
+            set.setDrawIcons(false); //아이콘 안 쓰겠다 선언
 
             set.setColor(Color.RED);
             set.setCircleColor(Color.RED);
@@ -105,7 +106,7 @@ public class GetLog extends GetRequest {
             LineData data = new LineData(set); // 데이터 셋 추가
 
             chart.setData(data);
-            chart.invalidate();
+            chart.invalidate(); //차트를 무효화했다가 다시 그리기 
         }
     }
     protected ArrayList<Tag> getArrayListFromJSONString(String jsonString) {
@@ -140,19 +141,20 @@ public class GetLog extends GetRequest {
         Log.i(TAG, "output="+output);
         return output;
     }
+        //Tag 클래스는 request해서 받아온 값들의 변수 선언을 위해 존재하며, 비율값, 타임스탬프값(문자열), Time값(밀리 세컨드), id(키)를 멤버 변수로 갖는다.
         class Tag {
             String Ratio;
             String Timestamp;
             String id;
             String Time;
-
+            //생성자 정의 및 초기화 부분
             public Tag(String deviceid, String ratio, String timestamp, String realTime) {
                 id = deviceid;
                 Ratio = ratio;
                 Timestamp = timestamp;
                 Time = realTime;
             }
-
+            //받아온 값들을 가지고 문자열로 만든다. -> 로그 조회 시에 활용
             public String toString() {
                 return String.format("{%s} [%s] ratio : %s", id, Timestamp, Ratio);
             }
